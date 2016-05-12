cb28f3e0a0edc3188e90467b33fb61c1bb68f8e2
Zhao Jingchen <dracupid@gmail.com>
2016-05-12 10:29:08 +0800

    fix error condition

--- dist/index.js
+++ dist/index.js
@@ -61,6 +61,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	  // avoid window.returnCitySN defined
 	  var conflictCache = typeof returnCitySN !== 'undefined' ? returnCitySN : undefined
 	  loadScript(API_MY_IP, {}, function (err) {
+	    if (typeof returnCitySN !== 'object') {
+	        return cb(new TypeError('Get IP failed.'))
+	    }
 	    var result = returnCitySN.cip || ''
 	    returnCitySN = conflictCache
 	    err == null ? cb(null, result) : cb(err)
--- index.js
+++ index.js
@@ -5,6 +5,9 @@ module.exports = function (cb) {
   // avoid window.returnCitySN defined
   var conflictCache = typeof returnCitySN !== 'undefined' ? returnCitySN : undefined
   loadScript(API_MY_IP, {}, function (err) {
+    if (typeof returnCitySN !== 'object') {
+        return cb(new TypeError('Get IP failed.'))
+    }
     var result = returnCitySN.cip || ''
     returnCitySN = conflictCache
     err == null ? cb(null, result) : cb(err)
